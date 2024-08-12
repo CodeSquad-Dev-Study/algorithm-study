83513284e554195724905211a7e8bc3bcd0372b2
yonghyeonpark <pyh4616@naver.com>
2024-08-12 12:30:17 +0900

    [BOJ_1541] 잃어버린괄호, 100ms, 14168KB

---
+++ yonghyeonpark/week1/first_attempt/BOJ_1541_잃어버린괄호_yonghyeonpark.java
@@ -0,0 +1,31 @@
+package yonghyeonpark.week1.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BOJ_1541_잃어버린괄호_yonghyeonpark {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] subtractionParts = br.readLine().split("-");
+        List<Integer> additionResults = new ArrayList<>();
+        for (String subtractionPart : subtractionParts) {
+            int sum = 0;
+            for (String additionPart : subtractionPart.split("\\+")) {
+                sum += Integer.parseInt(additionPart);
+            }
+            additionResults.add(sum);
+        }
+
+        int result = additionResults.get(0);
+        for (int i = 1; i < additionResults.size(); i++) {
+            result -= additionResults.get(i);
+        }
+
+        System.out.println(result);
+    }
+}
