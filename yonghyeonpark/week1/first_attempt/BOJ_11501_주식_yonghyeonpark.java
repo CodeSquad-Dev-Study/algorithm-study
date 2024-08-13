a8f028b4abcd6ef271c8685762edca06cf7dea57
yonghyeonpark <pyh4616@naver.com>
2024-08-13 18:07:15 +0900

    [BOJ_11501] 주식, 1088ms, 317360KB

---
+++ yonghyeonpark/week1/first_attempt/BOJ_11501_주식_yonghyeonpark.java
@@ -0,0 +1,39 @@
+package yonghyeonpark.week1.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11501_주식_yonghyeonpark {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int day = Integer.parseInt(br.readLine());
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] dailyPrices = new int[day];
+            for (int j = 0; j < day; j++) {
+                dailyPrices[j] = Integer.parseInt(st.nextToken());
+            }
+
+            int maxPrice = dailyPrices[day - 1];
+            long maxProfit = 0;
+            for (int j = day - 2; j >= 0; j--) {
+                if (maxPrice < dailyPrices[j]) {
+                    maxPrice = dailyPrices[j];
+                    continue;
+                }
+                maxProfit += maxPrice - dailyPrices[j];
+            }
+            sb.append(maxProfit)
+                    .append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
