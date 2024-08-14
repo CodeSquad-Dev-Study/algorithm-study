5183db5653bdd6a42c75e15179847a4357408cee
horang <fnelclsrn123@gmail,com>
2024-08-14 19:31:29 +0900

    [BOJ_2217] 로프, 292ms, 26920KB

---
+++ horanga/week1/first_attempt/BOJ_2217_로프_horanga.java
@@ -0,0 +1,29 @@
+package horanga.week1.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;;
+
+public class BOJ_2217_로프_horanga {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int max = 0;
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        //정렬
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N; i++) {
+            arr[i] *= (N - i);
+            if(max < arr[i]) max = arr[i];
+        }
+        System.out.println(max);
+    }
+}
