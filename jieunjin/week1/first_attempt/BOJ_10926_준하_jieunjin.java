9214fd6f89991af4f6989abdcec293d0bab7e4be
choejieun <wldms5221@icloud.com>
2024-08-18 23:35:56 +0900

    [BOJ_10926] ??!, 168ms, 17736KB

---
+++ jieunjin/week1/first_attempt/BOJ_10926_준하_jieunjin.java
@@ -0,0 +1,17 @@
+package jieunjin.week1.first_attempt;
+
+import java.util.Scanner;
+
+public class BOJ_10926_준하_jieunjin {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        if (input.equals(input)) {
+            System.out.println(input + "??!");
+        }
+    }
+}
