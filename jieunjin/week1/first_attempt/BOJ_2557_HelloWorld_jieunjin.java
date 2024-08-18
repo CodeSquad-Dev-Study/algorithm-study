7e490f33ee6ec907e9b7aeb03f1f3814714e4a6f
choejieun <wldms5221@icloud.com>
2024-08-18 23:31:53 +0900

    [BOJ_2557] Hello World, 84ms, 13844KB

---
+++ jieunjin/week1/first_attempt/BOJ_2557_HelloWorld_jieunjin.java
@@ -0,0 +1,8 @@
+package jieunjin.week1.first_attempt;
+
+public class BOJ_2557_HelloWorld_jieunjin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
