675cca3b09b84fb758b13e21f75ff93bb0805448
seondays <agseon9@gmail.com>
2024-08-21 01:00:54 +0900

    [BOJ_11724] 연결 요소의 개수, 1160ms, 209512KB

---
+++ seondays/week2/first_attempt/BOJ_11724_연결_요소의_개수_seondays.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ_11724_연결_요소의_개수_seondays {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    st = new StringTokenizer(br.readLine());
+
+    int vertexCount = Integer.parseInt(st.nextToken());
+    int lineCount = Integer.parseInt(st.nextToken());
+
+    Map<Integer, Set<Integer>> graph = new HashMap<>();
+    Set<Integer> visit = new HashSet<>();
+    int result = 0;
+
+    for (int i = 1; i <= vertexCount; i++) {
+      graph.put(i,new HashSet<>());
+    }
+
+    for(int i = 0; i < lineCount; i++) {
+      st = new StringTokenizer(br.readLine());
+      int start = Integer.parseInt(st.nextToken());
+      int end = Integer.parseInt(st.nextToken());
+      graph.computeIfAbsent(start, k -> new HashSet<>()).add(end);
+      graph.computeIfAbsent(end, k -> new HashSet<>()).add(start);
+    }
+
+    for (int i = 1; i <= vertexCount; i++) {
+      if (!visit.contains(i)) {
+        dfs(graph, i, visit);
+        result++;
+      }
+    }
+    System.out.println(result);
+  }
+
+  public static void dfs(Map<Integer, Set<Integer>> graph, int nowVertex, Set<Integer> visit) {
+    if (!visit.contains(nowVertex)) {
+      visit.add(nowVertex);
+      for(int nextVertex : graph.get(nowVertex)) {
+        if(!visit.contains(nextVertex)) {
+          dfs(graph, nextVertex, visit);
+        }
+      }
+    }
+  }
+}
