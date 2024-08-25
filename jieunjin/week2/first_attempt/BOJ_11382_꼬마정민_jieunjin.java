c708775f6dba1239ba6b6fd019abdcea146b3ca8
choejieun <wldms5221@icloud.com>
2024-08-25 09:58:22 +0900

    [BOJ_11382] 꼬마정민, 168ms, 17724KB

---
+++ jieunjin/week2/first_attempt/BOJ_11382_꼬마정민_jieunjin.java
@@ -0,0 +1,19 @@
+package jieunjin.week2.first_attempt;
+
+import java.util.Scanner;
+
+public class BOJ_11382_꼬마정민_jieunjin {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+
+
+        System.out.println(A + B + C);
+
+    }
+}
