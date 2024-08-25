7bfe7e48f8928f54c081c74fcd284f607ae38cc2
choejieun <wldms5221@icloud.com>
2024-08-25 10:38:53 +0900

    [BOJ_9498] 시험성적, 172ms, 17700KB

---
+++ jieunjin/week2/first_attempt/BOJ_9498_시험성적_jieunjin.java
@@ -0,0 +1,19 @@
+package jieunjin.week2.first_attempt;
+
+import java.util.Scanner;
+
+public class BOJ_9498_시험성적_jieunjin {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int A = in.nextInt();
+        in.close();
+
+        if (A >= 90) System.out.println("A");
+        else if (A >= 80) System.out.println("B");
+        else if (A >= 70) System.out.println("C");
+        else if (A >= 60) System.out.println("D");
+        else System.out.println("F");
+    }
+}
+
