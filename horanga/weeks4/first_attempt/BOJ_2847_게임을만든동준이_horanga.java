5baf838c343b24db5d6d4ed2cb294ffa874b534c
horang <fnelclsrn123@gmail,com>
2024-09-05 00:48:47 +0900

    [BOJ_2847] 게임을 만든 동준이, 192ms, 17776KB

---
+++ horanga/weeks4/first_attempt/BOJ_2847_게임을만든동준이_horanga.java
@@ -0,0 +1,31 @@
+package horanga.weeks4.first_attempt;
+
+import java.util.Scanner;
+public class BOJ_2847_게임을만든동준이_horanga {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        if(n==1){
+            System.out.println(0);
+            return;
+        }
+
+        int[] arr = new int[n];
+        for(int i =0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        int count=0;
+        for(int i =n-1; i>0; i--){
+            while(arr[i]<=arr[i-1]){
+                count++;
+                arr[i-1] = arr[i-1] -1;
+            }
+
+        }
+
+        System.out.println(count);
+    }
+}
