fe32b99fa70b688f2e405bdc4a6b7636c89134d0
seondays <agseon9@gmail.com>
2024-09-08 23:14:25 +0900

    [BOJ_15652] N과 M(4), 208ms ,16464KB

---
+++ seondays/week4/first_attempt/BOJ_15652_N과M4_seondays.java
@@ -0,0 +1,42 @@
+package week4.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15652_N과M4_seondays {
+
+    static int n;
+    static int m;
+    static int[] sequence;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        sequence = new int[m + 1];
+
+        solution(1, 1);
+    }
+
+    static void solution(int depth, int now) {
+        if (depth - 1 == m) {
+            StringBuffer sb = new StringBuffer();
+            for (int i = 1; i <= m; i++) {
+                sb.append(sequence[i]).append(" ");
+            }
+            System.out.println(sb);
+            return;
+        }
+
+        for (int i = now; i < n + 1; i++) {
+            if (sequence[depth - 1] <= i) {
+                sequence[depth] = i;
+                solution(depth + 1, now);
+            }
+        }
+    }
+}
