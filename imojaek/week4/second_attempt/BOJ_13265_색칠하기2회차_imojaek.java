50eb6a529f16dc11c311743717c5b6887c88bc79
imojaek <imojaek@gmail.com>
2024-09-10 12:06:37 +0900

    [BOJ_13265_2회차] 색칠하기, 576ms, 142720KB
    입력방식을 Scanner 에서 BufferedReader로 변경했더니 굉장한 성과

---
+++ imojaek/week4/second_attempt/BOJ_13265_색칠하기2회차_imojaek.java
@@ -0,0 +1,74 @@
+package imojaek.week4.second_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_13265_색칠하기2회차_imojaek {
+    static int n, m;
+
+    static int[] color;
+    static List<List<Integer>> graph;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        for (int t = 0; t < testCase; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+
+            color = new int[n + 1];
+            graph = new ArrayList<>();
+            for (int i = 0; i < n + 1; i++) {
+                graph.add(new ArrayList<>());
+            }
+
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int p1 = Integer.parseInt(st.nextToken());
+                int p2 = Integer.parseInt(st.nextToken());
+                graph.get(p1).add(p2);
+                graph.get(p2).add(p1);
+            }
+
+            boolean result = true;
+            for (int i = 1; i < n + 1; i++) {
+                if (color[i] == 0) {
+                    if (bfs(i) == false) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            if (result) {
+                System.out.println("possible");
+            } else {
+                System.out.println("impossible");
+            }
+        }
+    }
+
+    public static boolean bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        color[start] = 1; // 시작 색깔은 1, 다음 색깔은 2
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (Integer next : graph.get(current)) {
+                if (color[next] == 0) {
+                    if (color[current] == 1) {
+                        color[next] = 2;
+                    } else {
+                        color[next] = 1;
+                    }
+                    queue.add(next);
+                }
+                else if (color[next] == color[current]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
