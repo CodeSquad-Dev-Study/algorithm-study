6c079628aebf8d6ddb9f8bddd4992423148d1c41
seondays <agseon9@gmail.com>
2024-09-15 22:32:33 +0900

    [BOJ_2504] 절댓값 합, 524ms, 29468KB

---
+++ seondays/week5/first_attempt/BOJ_절댓값_힙_seondays.java
@@ -0,0 +1,44 @@
+package week5.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class BOJ_절댓값_힙_seondays {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) > Math.abs(o2)) {
+                    return 1;
+                } else if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                } else {
+                    return -1;
+                }
+            }
+        };
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(comparator);
+
+        for (int i = 0; i < count; i++) {
+            int now = Integer.parseInt(br.readLine());
+            if (now == 0) {
+                Integer top = pq.poll();
+                if (top == null) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(top);
+                }
+            } else {
+                pq.offer(now);
+            }
+        }
+    }
+}
