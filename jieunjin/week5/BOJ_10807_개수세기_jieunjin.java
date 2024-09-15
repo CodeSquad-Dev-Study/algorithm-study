7520669fe9c3c636d4e4fcf4ee400c7557a7fe51
choejieun <wldms5221@icloud.com>
2024-09-15 20:21:03 +0900

    [BOJ_10807] 개수세기, 100ms, 14324KB

--- jieunjin/week5/BOJ_10807_개수세기_jieunjin.java
+++ jieunjin/week5/BOJ_10807_개수세기_jieunjin.java
@@ -18,7 +18,6 @@ public class BOJ_10807_개수세기_jieunjin {
         int[] arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        ;
 
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
