586c9a777560a58ed6ce409ac393536b6395f148
seondays <agseon9@gmail.com>
2024-09-15 23:00:10 +0900

    [BOJ_2504] 카드 정렬하기, 316ms, 26004KB

---
+++ seondays/week5/first_attempt/BOJ_1715_카드_정렬하기_seondays.java
@@ -0,0 +1,29 @@
+package week5.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_1715_카드_정렬하기_seondays {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int result = 0;
+
+        for (int i = 0; i < count; i++) {
+            queue.offer(Integer.parseInt(br.readLine()));
+        }
+
+        while(queue.size() > 1) {
+            int first = queue.poll();
+            int second = queue.poll();
+            int sum = first + second;
+            result += sum;
+            queue.offer(sum);
+        }
+        System.out.println(result);
+    }
+}
