41ee12bb9a584c147091617113613ad4730a2ac5
wontae <dnjsxo0616@gmail.com>
2024-09-20 18:40:17 +0900

    [BOJ_1715] 카드 정렬하기, 312ms, 25784KB

---
+++ dnjsxo0616/week6/first_attempt/BOJ_1715_카드정렬하기_dnjsxo0616.java
@@ -0,0 +1,30 @@
+package dnjsxo0616.week6.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_1715_카드정렬하기_dnjsxo0616 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=0; i<t; i++) {
+            pq.offer(Integer.parseInt(br.readLine()));
+        }
+
+        int ans = 0;
+
+        while(pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            int sum = a + b;
+            ans += sum;
+            pq.add(sum);
+        }
+
+        System.out.println(ans);
+    }
+}
