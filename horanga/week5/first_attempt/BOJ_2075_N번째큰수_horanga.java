a1beadc14ceec5a943d3536c4bfaf1d2486a7392
horang <fnelclsrn123@gmail,com>
2024-09-22 22:28:12 +0900

    [BOJ_2075] N번째 큰수, 2768ms, 375452KB

---
+++ horanga/week5/first_attempt/BOJ_2075_N번째큰수_horanga.java
@@ -0,0 +1,26 @@
+package horanga.week5.first_attempt;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_2075_N번째큰수_horanga {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        Queue<Integer> queue = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        for(int i =0; i<num; i++){
+            for(int j =0; j<num; j++){
+                queue.add(scanner.nextInt());
+            }
+        }
+        for(int i =0; i<num-1; i++){
+            queue.poll();
+        }
+
+        System.out.println(queue.poll());
+    }
+}
