652dce5538ce41ed7c8d75fce1599ac112ad3de4
wontae <dnjsxo0616@gmail.com>
2024-09-23 12:37:13 +0900

    [BOJ_13975] 파일 합치기3, 2536ms, 282720KB

---
+++ dnjsxo0616/week7/first_attempt/BOJ_13975_파일합치기3_dnjsxo0616.java
@@ -0,0 +1,32 @@
+package dnjsxo0616.week7.first_attempt;
+
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_13975_파일합치기3_dnjsxo0616 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        for(int i=0; i<t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            PriorityQueue<Long> pq = new PriorityQueue<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                pq.add(Long.parseLong(st.nextToken()));
+            }
+            long sum = 0;
+            while (pq.size() > 1) {
+                long a = pq.poll();
+                long b = pq.poll();
+                long ab = a+b;
+                sum += ab;
+                pq.add(ab);
+            }
+            bw.write(sum + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
