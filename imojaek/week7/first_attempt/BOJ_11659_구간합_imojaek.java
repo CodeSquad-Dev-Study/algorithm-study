4b64b620b41666ace8161c2cb2c4dc0558c6c5de
imojaek <imojaek@gmail.com>
2024-09-27 19:39:23 +0900

    [BOJ_11659] 구간 합 구하기 4, 548ms, 59732KB

---
+++ imojaek/week7/first_attempt/BOJ_11659_구간합_imojaek.java
@@ -0,0 +1,30 @@
+package imojaek.week7.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11659_구간합_imojaek {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        int[] arr = new int[N + 1];
+        input = br.readLine().split(" ");
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(input[i - 1]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            input = br.readLine().split(" ");
+            sb.append(arr[Integer.parseInt(input[1])] - arr[Integer.parseInt(input[0]) - 1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
