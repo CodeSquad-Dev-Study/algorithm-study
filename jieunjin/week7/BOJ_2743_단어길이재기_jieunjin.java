1d4a92a7c8a8696af2dfc9691c2de7802a143089
choejieun <wldms5221@icloud.com>
2024-09-29 00:22:51 +0900

    [BOJ_2743] 단어길이재기, 160 ms, 17592 KB

---
+++ jieunjin/week7/BOJ_2743_단어길이재기_jieunjin.java
@@ -0,0 +1,16 @@
+package jieunjin.week7;
+
+import java.util.Scanner;
+
+public class BOJ_2743_단어길이재기_jieunjin {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String st = sc.next();
+        sc.close();
+
+        System.out.println(st.length());
+
+    }
+}
