4c47a0ac4dcbf113fa4ff016fbdbe1bfc0b238da
choejieun <wldms5221@icloud.com>
2024-09-29 00:26:22 +0900

    [BOJ_11654] 아스키코드, 160 ms, 17604 KB

---
+++ jieunjin/week7/BOJ_11654_아스키코드_jieunjin.java
@@ -0,0 +1,14 @@
+package jieunjin.week7;
+
+import java.util.Scanner;
+
+public class BOJ_11654_아스키코드_jieunjin {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int ch = in.next().charAt(0);
+
+        System.out.print(ch);
+    }
+}
