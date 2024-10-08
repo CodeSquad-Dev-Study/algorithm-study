6be5cc448f4ef0f7784d9d8de866a103508f89a3
seondays <agseon9@gmail.com>
2024-10-08 21:39:43 +0900

    [BOJ_1182] 부분수열의 합 116ms, 14368KB

---
+++ seondays/week9/first_attempt/BOJ_1182_부분수열의_합_seondays.java
@@ -0,0 +1,46 @@
+package week9.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_1182_부분수열의_합_seondays {
+
+    private static int n;
+    private static int m;
+    private static int[] numbers;
+    private static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] condition = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf)
+                .toArray();
+        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf)
+                .toArray();
+        n = condition[0];
+        m = condition[1];
+        result = 0;
+
+        backtracking(0, 0);
+        if(m == 0) {
+            result--;
+        }
+
+        System.out.println(result);
+    }
+
+    private static void backtracking(int index, int sum) {
+        if (index == numbers.length) {
+            if (sum == m) {
+                result++;
+                return;
+            }
+            return;
+        }
+
+        backtracking(index + 1, numbers[index] + sum);
+
+        backtracking(index + 1, sum);
+    }
+}
