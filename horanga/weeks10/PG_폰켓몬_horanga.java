8920e70450160265ec92b7856c44a359ac1cc4a9
horang <fnelclsrn123@gmail,com>
2024-10-21 00:03:40 +0900

    [PG] 폰켓몬, horanga

---
+++ horanga/weeks10/PG_폰켓몬_horanga.java
@@ -0,0 +1,26 @@
+package horanga.weeks10;
+
+import java.util.*;
+public class PG_폰켓몬_horanga {
+
+
+    public int solution(int[] nums) {
+   Set<Integer> set = new HashSet<>();
+
+
+        for(int i =0; i<nums.length; i++){
+            set.add(nums[i]);
+        }
+        int max =0;
+
+        if(nums.length%2==0){
+            max = nums.length/2;
+
+        } else{
+            max = (nums.length+1)/2;
+        }
+
+        return set.size()<max? set.size(): max;
+
+    }
+}
