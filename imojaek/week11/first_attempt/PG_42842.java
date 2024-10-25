6f751eb1d4935a8ae5baf7b883dabad315176c32
imojaek <imojaek@gmail.com>
2024-10-25 14:16:38 +0900

    [PG_42842] 카펫

---
+++ imojaek/week11/first_attempt/PG_42842.java
@@ -0,0 +1,19 @@
+package imojaek.week11.first_attempt;
+
+public class PG_42842 {
+    class Solution {
+        public int[] solution(int brown, int yellow) {
+            int[] answer = {};
+            int range = brown / 2 + 2;
+            for (int y = 3; y <= range / 2; y++) {
+                int x = range - y;
+                int inside = (y - 2) * (x - 2);
+                if (inside == yellow) {
+                    answer = new int[]{x, y};
+                }
+
+            }
+            return answer;
+        }
+    }
+}
