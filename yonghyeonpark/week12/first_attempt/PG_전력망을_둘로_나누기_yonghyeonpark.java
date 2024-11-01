a2df3e76e6a0c4e746abb8132d93432dd222ab80
yonghyeonpark <pyh4616@naver.com>
2024-11-01 23:06:28 +0900

    [PG] 전력망을 둘로 나누기

---
+++ yonghyeonpark/week12/first_attempt/PG_전력망을_둘로_나누기_yonghyeonpark.java
@@ -0,0 +1,68 @@
+package yonghyeonpark.week12.first_attempt;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class PG_전력망을_둘로_나누기_yonghyeonpark {
+
+    private List<List<Integer>> networks;
+    private boolean[] v;
+
+    public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
+
+        int length = wires.length;
+        int index = 0;
+        while (index < length) {
+            networks = new ArrayList<>();
+            for (int i = 0; i <= n; i++) {
+                networks.add(new ArrayList<>());
+            }
+            v = new boolean[n + 1];
+
+            for (int i = 0; i < length; i++) {
+                if (i == index) continue;
+                int[] wire = wires[i];
+                int a = wire[0];
+                int b = wire[1];
+
+                networks.get(a).add(b);
+                networks.get(b).add(a);
+            }
+
+            List<Integer> result = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                if (v[i]) continue;
+                result.add(bfs(i));
+            }
+            answer = Math.min(answer, Math.abs(result.get(0) - result.get(1)));
+            index++;
+        }
+        return answer;
+    }
+
+    private int bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        v[start] = true;
+
+        int count = 1;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            List<Integer> network = networks.get(cur);
+            int size = network.size();
+            for (int i = 0; i < size; i++) {
+                int num = network.get(i);
+
+                if (v[num]) continue;
+                q.offer(num);
+                v[num] = true;
+                count++;
+            }
+        }
+        return count;
+    }
+}
