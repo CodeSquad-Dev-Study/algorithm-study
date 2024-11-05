733a3e62daf23b4fa96b1897ef82d4a4bb821858
yonghyeonpark <pyh4616@naver.com>
2024-11-05 15:58:22 +0900

    [PG] 모음사전

---
+++ yonghyeonpark/week13/first_attempt/PG_모음사전_yonghyeonpark.java
@@ -0,0 +1,23 @@
+package yonghyeonpark.week13.first_attempt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PG_모음사전_yonghyeonpark {
+
+    private String[] vowel = {"A", "E", "I", "O", "U"};
+    private List<String> words = new ArrayList<>();
+
+    public int solution(String word) {
+        dfs("");
+        return words.indexOf(word);
+    }
+
+    private void dfs(String s) {
+        words.add(s);
+        if (s.length() == 5) return;
+        for (int i = 0; i < 5; i++) {
+            dfs(s + vowel[i]);
+        }
+    }
+}
