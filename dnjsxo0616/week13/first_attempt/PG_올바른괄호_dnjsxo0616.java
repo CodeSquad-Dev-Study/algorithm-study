8654ca0e1aa99268cbb7bfaed669bc8f6ef12f91
wontae <dnjsxo0616@gmail.com>
2024-11-06 17:45:42 +0900

    [PG] 올바른 괄호

---
+++ dnjsxo0616/week13/first_attempt/PG_올바른괄호_dnjsxo0616.java
@@ -0,0 +1,41 @@
+package dnjsxo0616.week13.first_attempt;
+
+import java.util.Stack;
+
+public class PG_올바른괄호_dnjsxo0616 {
+    boolean solution(String s) {
+        boolean answer = false;
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            stack.add(s.charAt(i));
+        }
+
+        char start = stack.pop();
+        if (start == '(') {
+            return answer;
+        }
+
+        int count = 1;
+        while (!stack.isEmpty()) {
+            char c = stack.pop();
+            switch (c) {
+                case ')':
+                    count++;
+                    break;
+                case '(':
+                    count--;
+                    break;
+            }
+
+            if (count < 0) {
+                return answer;
+            }
+        }
+
+        if (count == 0) {
+            answer = true;
+        }
+        return answer;
+    }
+}
