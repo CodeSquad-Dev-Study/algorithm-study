1aed1770b14857bf05ba6f562fa4b798ceb4922b
seondays <agseon9@gmail.com>
2024-11-14 17:00:55 +0900

    [BOJ_1747] 소수&펠린드롬, 2500ms, 307924KB

---
+++ seondays/week14/first_attempt/BOJ_1747_소수_펠린드롬_seondays.java
@@ -0,0 +1,54 @@
+package week14.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class BOJ_1747_소수_펠린드롬_seondays {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        while (true) {
+            if (isPalindrome(n)) {
+                if (isPrime(n)) {
+                    System.out.println(n);
+                    break;
+                }
+            }
+            n++;
+        }
+    }
+
+    static boolean isPrime(int n) {
+        boolean[] prime = new boolean[n + 1];
+        Arrays.fill(prime, true);
+        prime[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (prime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        return prime[n];
+    }
+
+    static boolean isPalindrome(int n) {
+        String text = String.valueOf(n);
+        Stack<String> stack = new Stack<>();
+        for (String s : text.split("")) {
+            stack.push(s);
+        }
+        for (String s : text.split("")) {
+            if (!stack.pop().equals(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
