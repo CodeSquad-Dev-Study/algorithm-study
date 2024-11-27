f8f129dab101900c8100abe9cab3e041c5de1037
yonghyeonpark <pyh4616@naver.com>
2024-11-28 05:39:49 +0900

    [BOJ_7785] 회사에 있는 사람, 632ms, 50384KB

---
+++ yonghyeonpark/week16/first_attempt/BOJ_7785_회사에_있는_사람_yonghyeonpark.java
@@ -0,0 +1,34 @@
+package yonghyeonpark.week16.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_7785_회사에_있는_사람_yonghyeonpark {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        Map<String, String> state = new HashMap<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            state.put(st.nextToken(), st.nextToken());
+        }
+
+        List<String> remain = new ArrayList<>();
+        for (Map.Entry<String, String> entry : state.entrySet()) {
+            if (entry.getValue().equals("leave")) continue;
+            remain.add(entry.getKey());
+        }
+
+        remain.sort(Comparator.reverseOrder());
+        StringBuilder result = new StringBuilder();
+        for (String person : remain) {
+            result.append(person).append("\n");
+        }
+        System.out.println(result);
+    }
+}
