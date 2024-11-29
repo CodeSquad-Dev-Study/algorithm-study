72ac8cc243efad4d49538df044931867b596fb0c
yonghyeonpark <pyh4616@naver.com>
2024-11-30 00:03:57 +0900

    [BOJ_1620] 나는야 포켓몬 마스터 이다솜, 636ms, 94856KB

---
+++ yonghyeonpark/week16/first_attempt/BOJ_1620_나는야_포켓몬_마스터_이다솜_yonghyeonpark.java
@@ -0,0 +1,37 @@
+package yonghyeonpark.week16.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_1620_나는야_포켓몬_마스터_이다솜_yonghyeonpark {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        List<String> li = new ArrayList<>();
+        li.add("empty");
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            String name = br.readLine();
+            li.add(name);
+            map.put(name, i);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            String input = br.readLine();
+            try {
+                result.append(li.get(Integer.parseInt(input))).append("\n");
+            } catch (NumberFormatException e) {
+                result.append(map.get(input)).append("\n");
+            }
+        }
+        System.out.println(result);
+    }
+}
