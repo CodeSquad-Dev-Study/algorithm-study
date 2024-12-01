77265d59a3a436a89ea26985ab2bfa49d9614645
horanga <fnelclsrn123@gmail.com>
2024-12-01 23:51:48 +0900

    [BOJ]11478, 서로다른 부분문자열의 개수, 1976ms, 492660 kb

---
+++ horanga/weeks16/first_attempt/PG_서로다른부분문자열의개_horanga.java
@@ -0,0 +1,27 @@
+package horanga.weeks16.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PG_서로다른부분문자열의개_horanga {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                set.add(str.substring(i, j));
+            }
+        }
+
+        System.out.println(set.size());
+    }
+}
