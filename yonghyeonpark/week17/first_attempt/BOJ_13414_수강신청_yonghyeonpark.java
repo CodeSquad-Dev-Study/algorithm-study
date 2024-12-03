1a480ca0518582384e111a2812d41bb96857a5c4
yonghyeonpark <pyh4616@naver.com>
2024-12-03 23:31:05 +0900

    [BOJ_13414] 수강신청, 1440ms, 66744KB

---
+++ yonghyeonpark/week17/first_attempt/BOJ_13414_수강신청_yonghyeonpark.java
@@ -0,0 +1,30 @@
+package yonghyeonpark.week17.first_attempt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ_13414_수강신청_yonghyeonpark {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Map<String, Integer> map = new HashMap<>();
+        int k = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= l; i++) {
+            String id = br.readLine();
+
+            map.put(id, i);
+        }
+
+        map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .limit(k)
+                .forEach(entry -> System.out.println(entry.getKey()));
+    }
+}
